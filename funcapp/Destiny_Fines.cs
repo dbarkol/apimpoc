e2d0ea6aa9bf4399cf1c11079e058f29a6ff5845
Rob DeRosa <roder@microsoft.com>
2018-10-17 20:32:38 -0500

    I can haz updates

--- funcapp/Destiny_Fines.cs
+++ funcapp/Destiny_Fines.cs
@@ -1,11 +1,9 @@
 
-using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
-using Newtonsoft.Json;
 using System.Collections.Generic;
 using funcapp.Models;
 using System;
@@ -18,35 +16,37 @@ namespace funcapp.Aasp
         public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "aasp/Fines")]HttpRequest req, TraceWriter log)
         {
 			var list = GetMockFines();
-			return (ActionResult)new OkObjectResult(list);
+			return new OkObjectResult(list);
         }
 
 		static List<Fine> GetMockFines()
 		{
-			var list = new List<Fine>();
-			list.Add(new Fine
+			var list = new List<Fine>
 			{
-				FirstName = "Rob",
-				Amount = 0.1,
-				DateDue = DateTime.Now.AddDays(20),
-				DateIssued = DateTime.Now
-			});
+				new Fine
+				{
+					FirstName = "Rob",
+					Amount = 0.1,
+					DateDue = DateTime.Now.AddDays(20),
+					DateIssued = DateTime.Now
+				},
 
-			list.Add(new Fine
-			{
-				FirstName = "Lynn",
-				Amount = 1000,
-				DateDue = DateTime.Now.AddDays(3),
-				DateIssued = DateTime.Now
-			});
+				new Fine
+				{
+					FirstName = "Lynn",
+					Amount = 1000,
+					DateDue = DateTime.Now.AddDays(3),
+					DateIssued = DateTime.Now
+				},
 
-			list.Add(new Fine
-			{
-				FirstName = "David",
-				Amount = 16.54,
-				DateDue = DateTime.Now.AddDays(7),
-				DateIssued = DateTime.Now
-			});
+				new Fine
+				{
+					FirstName = "David",
+					Amount = 16.54,
+					DateDue = DateTime.Now.AddDays(7),
+					DateIssued = DateTime.Now
+				}
+			};
 
 			return list;
 		}
